cb4f8fa6699e09a1406988d531391ab8eec144e9
Daria-Lapadus <127118850+darialapadus@users.noreply.github.com>
2025-03-24 11:34:36 +0000

    fix issues

--- src/main/java/ro/unibuc/hello/security/SecurityConfig.java
+++ src/main/java/ro/unibuc/hello/security/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .antMatchers("/api/users").hasRole("ADMIN") 
 
                         .anyRequest().authenticated())
                 .httpBasic(httpBasic -> httpBasic.disable())
